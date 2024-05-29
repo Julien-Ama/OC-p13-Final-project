326e4ae193322dcabcfd7ff698fbe4155a7416e9
Ingle <inglesejulien@hotmail.fr>
2024-05-29 14:28:19 +0200

    test id stripe

--- store/views.py
+++ store/views.py
@@ -192,8 +192,8 @@ def delete_cart(request):
 def stripe_webhook(request):
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    # endpoint_secret = "whsec_1b32becf79f2bc60e1a4c2f6ae0bc317cfb49545f10c4835b1a0a90ba9303183"
-    endpoint_secret = "whsec_1YmVD5KqOYXv3PVzQ5EAalhQBrKyLfif"
+    endpoint_secret = "whsec_1b32becf79f2bc60e1a4c2f6ae0bc317cfb49545f10c4835b1a0a90ba9303183"
+    # endpoint_secret = "whsec_1YmVD5KqOYXv3PVzQ5EAalhQBrKyLfif"
     event = None
     print("111111111111111111")
 
